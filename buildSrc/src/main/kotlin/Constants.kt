6d38521b3cf4f4502d44fc739522504195d3e008
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2024-05-26 23:24:24 +0300

    Fix publish repo

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -5,6 +5,6 @@ object Constants {
     const val LOCAL_PROPERTIES = "local.properties"
     const val VERSIONS_PROPERTIES = "versions.properties"
     const val PUBLISH_VERSION = "PUBLISH_VERSION"
-    const val MAVEN_URL = "https://maven.pkg.github.com/Meta-CTO/analytics-kmm"
+    const val MAVEN_URL = "https://maven.pkg.github.com/Meta-CTO/kmm-firebase-extensions"
     const val PUBLISH_MAVEN_REPO_NAME = "Github"
 }
