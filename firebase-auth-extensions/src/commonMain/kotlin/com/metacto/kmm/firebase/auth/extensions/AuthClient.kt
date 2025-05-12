992c7fbff60465b47fbdf2fe11c93be1bd9eb2d9
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2025-05-12 20:23:15 +0300

    Fix override

--- firebase-auth-extensions/src/commonMain/kotlin/com/metacto/kmm/firebase/auth/extensions/AuthClient.kt
+++ firebase-auth-extensions/src/commonMain/kotlin/com/metacto/kmm/firebase/auth/extensions/AuthClient.kt
@@ -1,8 +1,11 @@
 package com.metacto.kmm.firebase.auth.extensions
 
 import com.metacto.kmm.auth.common.AuthOptions
+import com.metacto.kmm.auth.common.AuthenticationMetadata
 
 expect class AuthClient() : AuthProvider {
     fun init()
     fun setAuthOptions(options: AuthOptions)
+    override suspend fun signInWithApple(): AuthenticationMetadata?
+    override suspend fun signInWithGoogle(): AuthenticationMetadata?
 }
--- firebase-config-extensions/src/commonMain/kotlin/com/metacto/kmm/firebase/remoteconfig/FirebaseRemoteConfigsProvider.kt
+++ firebase-config-extensions/src/commonMain/kotlin/com/metacto/kmm/firebase/remoteconfig/FirebaseRemoteConfigsProvider.kt
@@ -5,4 +5,16 @@ import com.metacto.kmm.sharedpreferences.KmmPreference
 
 expect class FirebaseRemoteConfigsProvider(
     remoteConfigPreferences: KmmPreference,
-): RemoteConfigProvider
+): RemoteConfigProvider {
+    override suspend fun init(minFetchIntervalSeconds: Long)
+    override fun getString(key: String): String?
+    override fun getBoolean(key: String): Boolean?
+    override fun getInt(key: String): Int?
+    override fun getDouble(key: String): Double?
+    override fun getLong(key: String): Long?
+    override suspend fun forceGetString(key: String): String?
+    override suspend fun forceGetBoolean(key: String): Boolean?
+    override suspend fun forceGetInt(key: String): Int?
+    override suspend fun forceGetDouble(key: String): Double?
+    override suspend fun forceGetLong(key: String): Long?
+}
